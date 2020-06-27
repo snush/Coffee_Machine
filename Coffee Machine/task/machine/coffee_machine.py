19d18c986a23a60584e81e6dafc24204f4822f15
Sveta Ushakova <53977091+snush@users.noreply.github.com>
2020-06-28 00:08:07 +0300

    Update coffee_machine.py

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,58 +1,61 @@
-def remainder(resources):
-    print("The coffee machine has:\n{} of water\n{} of milk\n{} of coffee beans\n{} of disposable cups\n"
-          "{} of money".format(resources[0], resources[1], resources[2], resources[3], resources[4]))
+class CoffeeMachine():
+  def __init__(self):
+      self.resources = [400, 540, 120, 9, 550]
+      self.espresso = [250, 0, 16, 1, 4]
+      self.latte = [350, 75, 20, 1, 7]
+      self.cappuccino = [200, 100, 12, 1, 6]
 
 
-def buy():
-    espresso = [250, 0, 16, 1, 4]
-    latte = [350, 75, 20, 1, 7]
-    cappuccino = [200, 100, 12, 1, 6]
-
-    choice = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
-    if choice == "1":
-        make(espresso)
-    elif choice == "2":
-        make(latte)
-    elif choice == "3":
-        make(cappuccino)
+  def remainder(self):
+      print("The coffee machine has:\n{} of water\n{} of milk\n{} of coffee beans\n{} of disposable cups\n"
+            "{} of money".format(self.resources[0], self.resources[1], self.resources[2], self.resources[3], self.resources[4]))
 
 
-def make(coffee):
-    ingredients = ["water", "milk", "coffee beans", "cups"]
-    for i in range(4):
-        if resources[i] < coffee[i]:
-            print("Sorry, not enough {}!".format(ingredients[i]))
-            return
-    print("I have enough resources, making you a coffee!")
-    for i in range(4):
-        resources[i] -= coffee[i]
-    resources[4] += coffee[4]
+  def make(self, coffee):
+      ingredients = ["water", "milk", "coffee beans", "cups"]
+      for i in range(4):
+          if self.resources[i] < coffee[i]:
+              print("Sorry, not enough {}!".format(ingredients[i]))
+              return
+      print("I have enough resources, making you a coffee!")
+      for i in range(4):
+          self.resources[i] -= coffee[i]
+      self.resources[4] += coffee[4]
 
 
-def fill(resources):
-    resources[0] += int(input("Write how many ml of water do you want to add:"))
-    resources[1] += int(input("Write how many ml of milk do you want to add:"))
-    resources[2] += int(input("Write how many grams of coffee beans do you want to add:"))
-    resources[3] += int(input("Write how many disposable cups of coffee do you want to add:"))
+  def buy(self):
+      choice = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+      if choice == "1":
+          self.make(self.espresso)
+      elif choice == "2":
+          self.make(self.latte)
+      elif choice == "3":
+          self.make(self.cappuccino)
 
 
-def take(resources):
-    print("I gave you ${}".format(resources[4]))
-    resources[4] = 0
+  def fill(self):
+      self.resources[0] += int(input("Write how many ml of water do you want to add:"))
+      self.resources[1] += int(input("Write how many ml of milk do you want to add:"))
+      self.resources[2] += int(input("Write how many grams of coffee beans do you want to add:"))
+      self.resources[3] += int(input("Write how many disposable cups of coffee do you want to add:"))
 
 
-resources = [400, 540, 120, 9, 550]
-
-action = input("Write action (buy, fill, take, remaining, exit):")
-while action != "exit":
-    if action == "buy":
-        buy()
-    elif action == "fill":
-        fill(resources)
-    elif action == "take":
-        take(resources)
-    elif action == "remaining":
-        remainder(resources)
-    action = input("Write action (buy, fill, take, remaining, exit):")
+  def take(self):
+      print("I gave you ${}".format(self.resources[4]))
+      self.resources[4] = 0
 
 
+  def action(self):
+      action = input("Write action (buy, fill, take, remaining, exit):")
+      while action != "exit":
+        if action == "buy":
+            self.buy()
+        elif action == "fill":
+            self.fill()
+        elif action == "take":
+            self.take()
+        elif action == "remaining":
+            self.remainder()
+        action = input("Write action (buy, fill, take, remaining, exit):")
+        
+CoffeeMachine().action()
